488c6f1f2dfc620ab710def4d799310ea4e35acd
Daniel Armstrong <dany.armstrong90@gmail.com>
2022-09-24 09:51:26 +0800

    add github, linkedin and gmail links.

---
+++ packages/dapp/src/constants/Links.ts
@@ -0,0 +1,4 @@
+export const GMAIL = "mailto:dany.armstrong90@gmail.com";
+export const LINKEDIN = "https://www.linkedin.com/in/daniel-armstrong90/";
+export const GITHUB = "https://github.com/dany-armstrong";
+export const GITHUB_PROJECT = "https://github.com/dany-armstrong/eth-lending-market";
--- packages/dapp/src/layout/AppLayout.tsx
+++ packages/dapp/src/layout/AppLayout.tsx
@@ -1,23 +1,25 @@
 import {ReactNode} from "react";
 import {Col, Layout, Row} from "antd";
-import { Content, Footer, Header } from "antd/lib/layout/layout";
+import {Content, Footer, Header} from "antd/lib/layout/layout";
 import logo from "../images/logo.png";
 import AppMenu from "./AppMenu";
 import Account from "../components/Account";
 import {useWeb3React} from "@web3-react/core";
 import {ActiveNetwork} from "../constants/Network";
 import WrongNetwork from "../containers/WrongNetwork";
+import {GithubOutlined, GoogleOutlined, LinkedinOutlined} from "@ant-design/icons";
+import {GITHUB, GITHUB_PROJECT, GMAIL, LINKEDIN} from "../constants/Links";
 
 const AppLayout = ({children}: { children: ReactNode }) => {
     const {active, chainId} = useWeb3React();
     return (
         <Row>
             <Col span={24}>
-                <Layout style={{ minHeight: "100vh" }}>
+                <Layout style={{minHeight: "100vh"}}>
                     <Header>
                         <Row align="stretch" gutter={20}>
                             <Col>
-                                <img src={logo.src} width={40} height={40} alt="logo" />
+                                <img src={logo.src} width={40} height={40} alt="logo"/>
                             </Col>
                             <Col>
                                 <h1>
@@ -25,16 +27,22 @@ const AppLayout = ({children}: { children: ReactNode }) => {
                                 </h1>
                             </Col>
                             <Col flex="auto">
-                                <AppMenu />
+                                <AppMenu/>
                             </Col>
-                            <Col style={{ marginRight: 10 }}>
-                                <Account />
+                            <Col style={{marginRight: 10}}>
+                                <Account/>
+                            </Col>
+                            <Col>
+                                <div>
+                                    <a href={GITHUB_PROJECT} target="_blank" rel="noopener noreferrer"><GithubOutlined
+                                        style={{fontSize: 20}}/></a>
+                                </div>
                             </Col>
                         </Row>
                     </Header>
                     <Content>
                         {active && chainId == ActiveNetwork ? children :
-                            <WrongNetwork />
+                            <WrongNetwork/>
                         }
                     </Content>
                     <Footer
@@ -45,6 +53,11 @@ const AppLayout = ({children}: { children: ReactNode }) => {
                         }}
                     >
                         © 2022 All rights reserved by Daniel Armstrong.
+                        <a href={GMAIL} rel="noopener noreferrer"><GoogleOutlined style={{fontSize: 16, marginLeft: 20}}/></a>
+                        <a href={LINKEDIN} target="_blank" rel="noopener noreferrer"><LinkedinOutlined
+                            style={{fontSize: 16, marginLeft: 10}}/></a>
+                        <a href={GITHUB} target="_blank" rel="noopener noreferrer"><GithubOutlined
+                            style={{fontSize: 16, marginLeft: 10}}/></a>
                     </Footer>
                 </Layout>
             </Col>
